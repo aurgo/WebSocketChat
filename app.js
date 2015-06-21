cf381ce10690b2cd1912450e2d01913501300811
Armando Urdiales <aurgo@outlook.com>
2015-06-21 18:38:22 +0200

    Delete user on Close connection
    Delete user on Close connection

--- app.js
+++ app.js
@@ -29,20 +29,44 @@ wss.on('connection', function connection(ws) {
 
     var js_message = JSON.parse(message);
 
-    if (clients['ws_' + js_message.room] != null)
-    {
-      clients['ws_' + js_message.room].forEach(function each(client) {
+  	console.log(message);
 
-        if (client != undefined)
-           client.send(message);
+    if (clients != null && js_message && js_message.room && clients['ws_' + js_message.room] != null)
+    {
+       clients['ws_' + js_message.room].forEach(function each(client) {
+
+       if (client != undefined)
+       {
+          try
+          {
+             client.send(message);
+          }
+          catch (e)
+          {
+             console.log(e);
+          }
+       }
       });
 
     }
   });
 
-  ws.send('{"user":"Server","message":"Connected to the server..."}');
-});
+  ws.on('close', function close(client, second) {
+    console.log('disconnected');
 
+    var url = ws.upgradeReq.url.replace('/', '');
+
+    if (Object.prototype.toString.call( clients['ws_' + url] ) == '[object Array]' )
+    {
+      var element = clients['ws_' + url].indexOf(ws);
+      if (element != -1)
+         clients['ws_' + url].splice(element, 1);
+    }
+  });
+
+  ws.send('{"user":"System", "room" : "","message":"Connected to the server..."}');
+
+});
 
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
